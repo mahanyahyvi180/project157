7f739179189fc726a8ee8b2910d3aaddc9979068
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-30 19:47:46 +0330

    Add files via upload

---
+++ WindowsFormsApp47/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp47
+{
+   public class cMath
+    {
+        public int x;
+        public int y;
+
+        public void swap()
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public double cal(char c)
+        {
+            switch (c)
+            {
+                case '+':
+                    return x + y;
+                case '-':
+                    return x - y;
+                case '*':
+                    return x * y;
+                case '/':
+                    return (double)x / y;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp47/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp47
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cMath t = new cMath();
+            t.x = int.Parse(textBox1.Text);
+            t.y = int.Parse(textBox2.Text);
+            t.swap();
+            textBox1.Text = t.x.ToString();
+            textBox2.Text = t.y.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char c = char.Parse(textBox3.Text);
+            cMath t = new cMath();
+            t.x = int.Parse(textBox1.Text);
+            t.y = int.Parse(textBox2.Text);
+
+            label2.Text = t.cal(c).ToString();
+        }
+    }
+}
